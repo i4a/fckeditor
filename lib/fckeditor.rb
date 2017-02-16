671f26db7f7fd34b391298d2842ef227fa2f3942
Mohamed Ziata <wakematta@gmail.com>
2017-02-16 16:39:14 +0100

    :construction: Make FCKeditor work in asses pipeline

--- lib/fckeditor.rb
+++ lib/fckeditor.rb
@@ -12,31 +12,36 @@ module Fckeditor
         instance_variable_set("@#{object}", eval("#{klass}.new()"))
       end
       id = fckeditor_element_id(object, field)
-      
+
       cols = options[:cols].nil? ? '' : "cols='"+options[:cols]+"'"
       rows = options[:rows].nil? ? '' : "rows='"+options[:rows]+"'"
-      
+
       width = options[:width].nil? ? '100%' : options[:width]
       height = options[:height].nil? ? '100%' : options[:height]
-      
+
       toolbarSet = options[:toolbarSet].nil? ? 'Default' : options[:toolbarSet]
-      
+
       if options[:ajax]
         inputs = "<input type='hidden' id='#{id}_hidden' name='#{object}[#{field}]'>\n" <<
                  "<textarea id='#{id}' #{cols} #{rows} name='#{id}'>#{value}</textarea>\n"
-      else 
+      else
         inputs = "<textarea id='#{id}' #{cols} #{rows} name='#{object}[#{field}]'>#{value}</textarea>\n"
       end
-      
-      js_path = "/javascripts"
-      base_path = "#{js_path}/fckeditor/"
+
+      js_path   = asset_path('')
+      base_path = asset_path('fckeditor/')
+
       return inputs.html_safe <<
-        javascript_tag("var oFCKeditor = new FCKeditor('#{id}', '#{width}', '#{height}', '#{toolbarSet}');\n" <<
-                       "oFCKeditor.BasePath = \"#{base_path}\"\n" <<
-                       "oFCKeditor.Config['CustomConfigurationsPath'] = '#{js_path}/fckcustom.js';\n" <<
-                       "oFCKeditor.ReplaceTextarea();\n").html_safe
+        javascript_tag(
+        <<-JAVASCRIPT
+          var oFCKeditor = new FCKeditor('#{id}', '#{width}', '#{height}', '#{toolbarSet}');
+          oFCKeditor.BasePath = "#{base_path}";
+          oFCKeditor.Config['CustomConfigurationsPath'] = '= #{js_path}/fckcustom.js';
+          oFCKeditor.ReplaceTextarea();
+        JAVASCRIPT
+        )
     end
-    
+
     def fckeditor_form_remote_tag(options = {})
       editors = options[:editors]
       before = ""
@@ -45,10 +50,10 @@ module Fckeditor
           before += fckeditor_before_js(e, f)
         end
       end
-      options[:before] = options[:before].nil? ? before : before + options[:before] 
+      options[:before] = options[:before].nil? ? before : before + options[:before]
       form_remote_tag(options)
     end
-    
+
     def fckeditor_remote_form_for(object_name, *args, &proc)
       options = args.last.is_a?(Hash) ? args.pop : {}
       concat(fckeditor_form_remote_tag(options), proc.binding)
@@ -56,22 +61,24 @@ module Fckeditor
       concat('</form>', proc.binding)
     end
     alias_method :fckeditor_form_remote_for, :fckeditor_remote_form_for
-    
+
     def fckeditor_element_id(object, field)
       id = eval("@#{object}.id")
-      "#{object}_#{id}_#{field}_editor"    
+      "#{object}_#{id}_#{field}_editor"
     end
 
     def fckeditor_div_id(object, field)
-      id = eval("@#{object}.id")  
-      "div-#{object}-#{id}-#{field}-editor" 
+      id = eval("@#{object}.id")
+      "div-#{object}-#{id}-#{field}-editor"
     end
 
     def fckeditor_before_js(object, field)
       id = fckeditor_element_id(object, field)
       "var oEditor = FCKeditorAPI.GetInstance('"+id+"'); document.getElementById('"+id+"_hidden').value = oEditor.GetXHTML();"
-    end    
+    end
   end
+
+  ActionView::Base.send(:prepend, ActionViewMods)
 end
 
 require 'railtie' if defined?(Rails)
--- lib/fckeditor_version.rb
+++ lib/fckeditor_version.rb
@@ -1,9 +1,9 @@
 module FckeditorVersion
-  MAJOR = 0
-  MINOR = 6
-  RELEASE = 2
-  
+  MAJOR = 1
+  MINOR = 0
+  RELEASE = 0
+
   def self.current
-  	"#{MAJOR}.#{MINOR}.#{RELEASE}"
+    "#{MAJOR}.#{MINOR}.#{RELEASE}"
   end
 end
